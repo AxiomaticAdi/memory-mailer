13487e7ffe9d166cc1d8210edb4f937d30fcc52c
AxiomaticAdi <arudra@bu.edu>
2024-09-09 23:02:32 -0600

    Fix splash page positioning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import FeatureDescription from "@/components/FeatureDescription";
 
 export default function home() {
 	return (
-		<main className="flex flex-1 w-full justify-center items-center">
+		<main className="flex flex-1 w-full justify-center items-center min-h-[100dvh]">
 			<section className="w-full flex justify-center items-center py-12 md:py-24 lg:py-32">
 				<div className="container px-4 md:px-6 grid gap-8 lg:grid-cols-2 lg:gap-12">
 					<div className="space-y-4">
